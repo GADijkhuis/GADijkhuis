d70772092b5a43d728541a0d67513e6b2a1092e7
Gideon Dijkhuis <dijkhuis.g.a@outlook.com>
2026-08-13 09:08:27 +0200

    feat: Header components and first layout

---
+++ gadijkhuis/src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from "@angular/common";
+import { ButtonComponent } from "../button/button.component";
+
+@Component({
+  selector: 'app-header',
+  imports: [NgOptimizedImage, ButtonComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+
+}
